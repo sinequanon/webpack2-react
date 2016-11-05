3637d8f86a72b44f502b234a9671554a1c94f380
Rowell Sotto <github@sotto.org>
2016-11-05 16:22:18 -0700

    Supporting additional loaders
    CSS loaders. Looks like webpack removed support for addDepedency so if
    you edit a css file that is imported, the app won't hot reload unless
    you also make an edit to the main css file that is referenced. Hopefully
    the import loaders we use will work with whatever changes have been made
    in webpack 2 to support this.

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -1,109 +1,105 @@
-import webpack from 'webpack'
 import path from 'path'
 import fs from 'fs'
-import HtmlWebPackPlugin from 'html-webpack-plugin'
-import ExtractTextPlugin from 'extract-text-webpack-plugin'
-import optimizeCssAssets from 'optimize-css-assets-webpack-plugin'
 
+import webpack from 'webpack'
+// Emits html files injected with asset tags
+import HtmlWebPackPlugin from 'html-webpack-plugin'
+// Used to output css file in PROD build
+import ExtractTextPlugin from 'extract-text-webpack-plugin'
+// Fixes possible dupes in ExtractTextPlugin 
+import optimizeCssAssets from 'optimize-css-assets-webpack-plugin'
 // Plugin for using cssnext features
 import postcssnext from 'postcss-cssnext'
 // Use precss as replacement for sass lib due to apparent speed up and much 
 // faster compilation using postcss
 import precss from 'precss'
-// Include postcss-import to fix a hot loading bug in imported css files
-// import postcssImport from 'postcss-import'
+// Include postcss-smart-import to fix a hot loading bug in imported css files
+import postcssImport from 'postcss-smart-import'
 // Reporter for postcss process
 import postcssReporter from 'postcss-reporter'
-import smartImport from 'postcss-smart-import'
 
-const babelPath = path.resolve(__dirname, '..', '.babelrc')
-const babelrc = fs.readFileSync(babelPath)
-let babelOptions = {}
+// Webpack 2 now allows passing the -p parameter which auto sets NODE_ENV to
+// 'production'. While convenient, it isn't explicit so we manually set the 
+// variable in the npm prod script
+const IS_PROD = process.env.NODE_ENV === 'production'
 
-try {
-  babelOptions = JSON.parse(babelrc)
-} catch (e) {
-  console.error('==> Error parsing .babelrc')
-  console.error(e)
-}
-// remove es2015
-babelOptions.presets.shift()
+export const appEntry = [].concat(
+  !IS_PROD ? ['react-hot-loader/patch'] : [],
+  [ path.resolve(__dirname, '../src', 'client.js')])
 
-export const cssLoaders = [
-  {
-    loader : 'style-loader'
-  },
-  {
-    loader : 'css-loader',
-    query : {
-      sourceMap : true
-    }
-  },
-  {
-    loader : 'postcss-loader',
-    query : {
-      sourceMap : true
-    }
+export const babelOptions = (() => {
+  const babelPath = path.resolve(__dirname, '..', '.babelrc')
+  const babelrc = fs.readFileSync(babelPath)
+  let babelOptions = {}
+
+  try {
+    babelOptions = JSON.parse(babelrc)
+  } catch (e) {
+    console.error('==> Error parsing .babelrc')
+    console.error(e)
   }
-]
 
-// Make a copy of the loaders array
-const cssLoadersCopy = cssLoaders.slice()
-// Remove the first item from the array. It will cause a window undefined error
-// if you use it with the extract text plugin
-cssLoadersCopy.shift()
+  // babelrc includes the ES2015 preset so we can write webpack config using
+  // ES2015 code. However we can remove ES2015 once the file is loaded since 
+  // Chrome supports most ES2015 natively
+  babelOptions.presets.shift()
+  return babelOptions
+})()
 
-export const commonConfig = {
-  entry : {
-    app: [
-      path.resolve(__dirname, '../src', 'client.js'),
-    ],
-    common : path.resolve(__dirname, '../src', 'vendor.js')
-  },
-  output : {
-    path: path.resolve(__dirname, '..', 'dist'),
-    filename      : '[name].js',
-    chunkFilename : '[id].js',
-    // The logical path in the browser
-    publicPath    : '/'
-  },
-  resolve : {
-    modules : [ 'node_modules' ]
-  },
-  module : {
-    rules : [
-      {
-        test    : /\.jsx?$/,
-        exclude : /node_modules/,
-        // Disable react hot module loading in PROD
-        loader : 'babel',
-        options : babelOptions
-      },
-      {
-        test   : /\.s?css$/,
-        // use : cssLoaders
-        loader : ExtractTextPlugin.extract({
-          fallbackLoader : 'style-loader',
-          loader : cssLoadersCopy//cssLoaders.slice().shift()  // remove the first element
-        })
-      },
-    ]
-  },
-  target: 'web',
-  plugins: [
-    // only use in production
-    // new webpack.optimize.DedupePlugin(),
-    // Emits css file using naming pattern below
-    new ExtractTextPlugin({ filename: '[name]-[contenthash].css'}),
+export const styleLoader = (() => {
+  // Style loaders normally used in DEVELOPMENT
+  const styleLoaders = [
+    {
+      loader : 'style'
+    },
+    {
+      loader : 'css',
+      query : {
+        sourceMap : true
+      }
+    },
+    {
+      loader : 'postcss',
+      query : {
+        sourceMap : true
+      }
+    }
+  ]
+
+  let styleLoader = styleLoaders
+  if (IS_PROD) {
+    // Remove the style loader (first loader in the styleLoaders array) from 
+    // the styleLoaders in a PROD build since having it present in the 
+    // ExtractTextPlugin will cause a window undefined error
+    styleLoaders.shift()
+
+    // In PRODUCTION extract the CSS
+    styleLoader = ExtractTextPlugin.extract({
+      fallbackLoader : 'style',
+      loader : styleLoaders
+    })
+  }
+  return styleLoader
+})()
+
+export const plugins = (() => {
+  let currentPlugins = [
+    // Defines environment variable that will be trigger a pared down
+    // version of React to be exercised during minification. NOTE: we
+    // already specify the NODE_ENV=production in the package.json
+    // script However that doesn't appear to trigger the minifier to
+    // compress the production version which doesn't include debugging
+    // info, so we must explicitly set the environment variable with the
+    // plugin.
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV':  JSON.stringify(process.env.NODE_ENV)
+    }),
     new webpack.LoaderOptionsPlugin({
       options : {
         context : '/',
         postcss : function (webpack) {
           return [ 
-            // require('postcss-import')({ addDependencyTo: webpack }),
-            // smartImport({ addDependencyTo: webpack }),
-            smartImport(),
-            //smartImport({ addDependencyTo: webpack }),
+            postcssImport({ skipDuplicates: true }),
             // Allow sass syntax in css
             precss,
             // Allow next gen css syntax
@@ -115,8 +111,8 @@ export const commonConfig = {
       }
     }),
     new webpack.optimize.CommonsChunkPlugin({
-        minChunks: 2,
-        name: 'common'
+      minChunks: 2,
+      name: 'common'
     }),
     new webpack.NamedModulesPlugin(),
     new HtmlWebPackPlugin({ 
@@ -135,4 +131,23 @@ export const commonConfig = {
     }),
     new optimizeCssAssets()
   ]
-}
+  
+  if (IS_PROD) {
+    currentPlugins = currentPlugins.concat([
+      // Emits css file using naming pattern below
+      new ExtractTextPlugin('[name]-[contenthash].css'),
+      new webpack.optimize.UglifyJsPlugin({
+        comments : false,
+        compress : {
+          screw_ie8 : true,
+          warnings : true,
+        }
+      }),
+      // Prevents emitting assets that include errors in them
+      // Do not include in non PROD environments otherwise lint 
+      // warnings will fail to emit anything to the build
+      new webpack.NoErrorsPlugin()
+    ])
+  }
+  return currentPlugins
+})()
--- config/webpack.config.babel.js
+++ config/webpack.config.babel.js
@@ -1,17 +1,51 @@
-import webpack from 'webpack'
 import path from 'path'
-import merge from 'webpack-merge'
-import HtmlWebPackPlugin from 'html-webpack-plugin'
 
-import { cssLoaders, commonConfig } from './webpack.common'
+import { appEntry, babelOptions, styleLoader, plugins } from './webpack.common'
 
-const merged = merge({
-  entry: {
-    app: [
-      'react-hot-loader/patch'
+const IS_PROD = process.env.NODE_ENV === 'production'
+
+export default {
+  entry : {
+    app: appEntry,
+    common : path.resolve(__dirname, '../src', 'vendor.js')
+  },
+  output : {
+    path: path.resolve(__dirname, '..', 'dist'),
+    filename      : '[name].js',
+    chunkFilename : '[id].js',
+    // The logical path in the browser
+    publicPath    : '/'
+  },
+  resolve : {
+    modules : [ 'node_modules' ]
+  },
+  module : {
+    rules : [
+      {
+        test    : /\.jsx?$/,
+        exclude : /node_modules/,
+        loader : 'babel',
+        options : babelOptions
+      },
+      {
+        test   : /\.s?css$/,
+        loader : styleLoader
+      },
+      {
+        // Image assets
+        test   : [/\.png/, /\.jpg$/, /\.gif$/],
+        // Any image assets will be autoconverted to inline base64
+        // unless they are over the limit specified in the query
+        // parameter. If the assets is over the limit, the
+        // file-loader takes over and will emit the file using the
+        // naming pattern specified. name options include [path]
+        // [name] [hash] [ext] In the case below, we are telling
+        // the loader to emit the file in the static directory
+        // using the file name followed by the hash and extension.
+        loader : 'url?limit=1&name=static/[name]-[hash].[ext]'
+      }
     ]
-  }
-}, commonConfig)
-
-console.log('merged', merged)
-export default merged
+  },
+  target: 'web',
+  plugins: plugins
+}
